3881d6d74c8e7494759280fd61c439853b678f01
MichaelHWatkins <michaelhugheswatkins@gmail.com>
2021-11-10 11:20:41 -0800

    complete the first test...
    Co-Authored-by: Matt Luker <jamesmattluker@gmail.com>
    
    Co-authored-by: Liz Thomas <thomas.elizabeth.k@gmail.com>

--- src/js/puppy.js
+++ src/js/puppy.js
@@ -1,6 +1,9 @@
 export default class Puppy {
   
   constructor(name, cutenessPoints, energyPoints, affectionPoints) {
-    
+    this.name = name;
+    this.cutenessPoints = cutenessPoints;
+    this.energyPoints = energyPoints;
+    this.affectionPoints = affectionPoints; 
   }    
 }
